f0e28599659d7ab17515ada64e9d5dd5f61fc097
MrAviator <georgemcgrath10@outlook.com>
2026-04-23 17:07:32 +0100

    Create http.py

---
+++ http.py
@@ -0,0 +1,16 @@
+import aiohttp
+
+API = "https://discord.com/api/v10"
+
+
+async def send_message(channel_id, token, payload):
+    url = f"{API}/channels/{channel_id}/messages"
+
+    headers = {
+        "Authorization": f"Bot {token}",
+        "Content-Type": "application/json"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=payload, headers=headers) as resp:
+            return await resp.text()
